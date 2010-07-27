64adfcb2053cdfc37a26357c1088564b06e40303
Jay Zhang <ghosert@gmail.com>
2010-07-27 17:03:06 +0800

    add 6_Heapsort.c

---
+++ Introduction_To_Algorithms/6_Heapsort.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+
+void max_heapify(int *, int);
+
+typedef struct tnode {
+    int value;
+    Treenode *left;
+    Treenode *right;
+} Treenode;
+
+void max_heapify(int *ap, int i)
+{
+}
