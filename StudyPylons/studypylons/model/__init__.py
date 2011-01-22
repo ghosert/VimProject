60fe58c896856d040c643c10befb7f848ebe63ba
jiawzhang <ghosert@gmail.com>
2011-01-22 21:24:44 +0800

    add SQLAlchemy samples.

--- StudyPylons/studypylons/model/__init__.py
+++ StudyPylons/studypylons/model/__init__.py
@@ -1,6 +1,9 @@
 """The application's model objects"""
 from studypylons.model.meta import Session, Base
 
+# Add model/person.py, model/address.py first, then import its class Person, Address here.
+from studypylons.model.person import Person
+from studypylons.model.address import Address
 
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
