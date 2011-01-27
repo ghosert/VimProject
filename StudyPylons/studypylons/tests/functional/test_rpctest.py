9f1b4f29004d76fcf0bd6d486b66dfadd61233b0
Jay Zhang <ghosert@gmail.com>
2011-01-27 13:54:51 +0800

    delete test_rpctest.py

--- StudyPylons/studypylons/tests/functional/test_rpctest.py
+++
@@ -1,7 +0,0 @@
-from studypylons.tests import *
-
-class TestRpctestController(TestController):
-
-    def test_index(self):
-        response = self.app.get(url(controller='rpctest', action='index'))
-        # Test response...
