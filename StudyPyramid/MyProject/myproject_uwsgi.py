4289ca7ff9e31f0d335568bad79d29cd96708f43
Jay Zhang <ghosert@gmail.com>
2011-06-16 17:03:44 +0800

    logging suports.

--- StudyPyramid/MyProject/myproject_uwsgi.py
+++ StudyPyramid/MyProject/myproject_uwsgi.py
@@ -1,6 +1,11 @@
 # This py will be invoked in uwsgi.xml
 
+# Testing: For uwsgi file logging from pyramid, the location of the log file is defined in production.ini
+from paste.script.util.logging_config import fileConfig
+fileConfig('/home/jiawzhang/VimProject/StudyPyramid/MyProject/production.ini')
+
 from pyramid.paster import get_app
 application = get_app(
   '/home/jiawzhang/VimProject/StudyPyramid/MyProject/production.ini', 'main')
+
 
