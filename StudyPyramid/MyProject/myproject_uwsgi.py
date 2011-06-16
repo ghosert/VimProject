bdbeba68a12b478617791bc233a1440cc822eeb1
jiawzhang <ghosert@gmail.com>
2011-06-16 21:45:55 +0800

    update logging

--- StudyPyramid/MyProject/myproject_uwsgi.py
+++ StudyPyramid/MyProject/myproject_uwsgi.py
@@ -1,11 +1,13 @@
 # This py will be invoked in uwsgi.xml
 
-# Testing: For uwsgi file logging from pyramid, the location of the log file is defined in production.ini
+ini_file = '/home/jiawzhang/VimProject/StudyPyramid/MyProject/production.ini'
+
+# For uwsgi file logging from pyramid, the location of the log file is defined in production.ini
+# Otherwise, starting application from uwsgi will not log anything to the log file which is defined in production.ini
 from paste.script.util.logging_config import fileConfig
-fileConfig('/home/jiawzhang/VimProject/StudyPyramid/MyProject/production.ini')
+fileConfig(ini_file)
 
+# Construct application here.
 from pyramid.paster import get_app
-application = get_app(
-  '/home/jiawzhang/VimProject/StudyPyramid/MyProject/production.ini', 'main')
-
+application = get_app(ini_file, 'main')
 
