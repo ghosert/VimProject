d06e65448b7d80e395c7a42b511f26fb39d7328c
jiawzhang <ghosert@gmail.com>
2012-06-09 23:55:29 +0800

    comment the line may cause error

--- StudyPython/briefStandardLibrary.py
+++ StudyPython/briefStandardLibrary.py
@@ -121,11 +121,11 @@ if __name__ == "__main__":
     print textwrap.fill(doc, width=40)
     
     import locale
-    locale.setlocale(locale.LC_ALL, 'English_United States.1252')
+    # locale.setlocale(locale.LC_ALL, 'English_United States.1252')
     conv = locale.localeconv() # get a mapping of conventions
     x = 1234567.8
     print locale.format("%d", x, grouping=True)
-    print locale.format("%s%.*f", (conv['currency_symbol'], conv['frac_digits'], x), grouping=True)
+    # print locale.format("%s%.*f", (conv['currency_symbol'], conv['frac_digits'], x), grouping=True)
 
     # templating
     from string import Template
