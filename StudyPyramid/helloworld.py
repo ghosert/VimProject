2926a31461093001ac438cbbd524e3a499cefcfb
Zhang Jiawei <ghosert@gmail.com>
2013-02-14 21:57:30 +0800

    update pyramid doc

--- StudyPyramid/helloworld.py
+++ StudyPyramid/helloworld.py
@@ -13,12 +13,20 @@ def goodbye_world(request):
 def hello(request):
     return Response('Hello Declarative Configuration!')
 
+@view_config(route_name = 'test_route_name')
+def test_route_name(request):
+    print request.matchdict['test']
+    return Response('This is a placement marker test.')
+
 if __name__ == '__main__':
     config = Configurator()
     config.add_view(hello_world) # Imperative Configuration, without 'name' specified, it means root context '/'.
     config.add_view(goodbye_world, name='goodbye')
+    config.add_route('test_route_name', '/test_route/{test}')
     # config.scan() will take care all the method with @view_config annotation, this is called declarative configuration.
-    # And it has same side effect to imperative way: config.add_view(hello, name='hello')
+    # And it has same side effect to imperative way:
+    # config.add_view(hello, name='hello')
+    # config.add_view(test_route_name, route_name='test_route_name')
     config.scan() # Declarative Configuration
     app = config.make_wsgi_app()
     server = make_server('0.0.0.0', 8080, app)
