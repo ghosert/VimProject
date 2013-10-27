0cd56c722524b41514f877b3483b0f9c95a6734d
Zhang Jiawei <ghosert@gmail.com>
2013-10-27 22:40:38 +0800

    sample of rabbitmq

---
+++ StudyPyramid/rabbitmq/receive.py
@@ -0,0 +1,20 @@
+# coding: utf-8
+
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        host='localhost'))
+channel = connection.channel()
+
+channel.queue_declare(queue='hello')
+
+print ' [*] Waiting for messages. To exit press CTRL+C'
+
+def callback(ch, method, properties, body):
+    print " [x] Received %r" % (body,)
+
+channel.basic_consume(callback,
+                      queue='hello',
+                      no_ack=True)
+
+channel.start_consuming()
