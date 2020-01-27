ec32cd93ede503b1c788733e696a95ef29a6ffcb
Zhang Jiawei <ghosert@gmail.com>
2020-01-26 21:58:21 -0800

    test al

---
+++ Algorithms/src/basic/TestAll.java
@@ -0,0 +1,11 @@
+package basic;
+
+import java.util.Stack;
+
+public class TestAll {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        System.out.println("hello stack pop: " + stack.pop());
+    }
+}
