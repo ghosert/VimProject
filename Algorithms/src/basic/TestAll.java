5e2a8fcbbec6a6e115fa0daa6d068467d43c226e
jiawzhang <jiawzhang@ebay.com>
2020-01-31 21:46:03 -0800

    add GenericsWildcards for test

--- Algorithms/src/basic/TestAll.java
+++ Algorithms/src/basic/TestAll.java
@@ -1,8 +1,6 @@
 package basic;
 
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Stack;
+import java.util.*;
 
 public class TestAll {
     public static void main(String[] args) {
@@ -12,5 +10,27 @@ public class TestAll {
         Stack<Integer> stack = new Stack<>();
         stack.push(1);
         System.out.println("stack is empty and value: " + stack.isEmpty() + " " + stack.pop());
+    }
+
+}
+
+class GenericsWildcards {
+
+    public static void main(String[] args) {
+        List<Integer> ints = new ArrayList<>();
+        ints.add(3); ints.add(5); ints.add(10);
+        double sum = sum(ints);
+        System.out.println("Sum of ints="+sum);
+    }
+
+    public static double sum(List<? extends Number> list){
+        double sum = 0;
+        for(Number n : list){
+            sum += n.doubleValue();
+        }
+        // You are not allowed to add new element to the upper bounded list except null:
+        // not working: Integer i = 2; list.add(i);
+        // working: list.add(null);
+        return sum;
     }
 }
